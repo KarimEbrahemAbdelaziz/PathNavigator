7be5b66eede02eca41b1ba81fe7eb2ca449a822c
Karim Ebrahem <karimabdelazizmansour@gmail.com>
2024-03-29 16:37:56 +0400

    Initial Commit

---
+++ PathNavigator/PathNavigator.h
@@ -0,0 +1,18 @@
+//
+//  PathNavigator.h
+//  PathNavigator
+//
+//  Created by karim ebrahim on 29/03/2024.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for PathNavigator.
+FOUNDATION_EXPORT double PathNavigatorVersionNumber;
+
+//! Project version string for PathNavigator.
+FOUNDATION_EXPORT const unsigned char PathNavigatorVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <PathNavigator/PublicHeader.h>
+
+
